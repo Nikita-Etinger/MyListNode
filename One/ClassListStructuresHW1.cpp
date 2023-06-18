79e888424fdbc64a6ba57d6b560a580e588c3b4c
Nikita Etinger <113473133+Nikita-Etinger@users.noreply.github.com>
2023-06-18 22:25:33 +0300

    Add files via upload

---
+++ One/ClassListStructuresHW1.cpp
@@ -0,0 +1,33 @@
+﻿#include <iostream>
+#include "ClassList.h"
+using namespace std;
+
+
+
+
+
+int main()
+{
+    List<int> myList;
+    myList.push(1);
+    myList.push(2);
+    myList.push(3);
+    myList.push(4);
+
+    myList.show(); 
+
+    myList.insert(10, 2);
+    myList.show(); 
+
+    myList.removeHead();
+    myList.show(); 
+
+    myList.removeAt(2);
+    myList.show();
+
+     cout << "Size: " << myList.size() <<  endl;
+     cout << "Is empty? " << (myList.empty() ? "Yes" : "No") <<  endl;
+
+    return 0;
+
+};
